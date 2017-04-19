e02e0c02bcbd62e838b037478854be3729e20b2a
jamaalg <jamaal1087@gmail.com>
2017-04-19 13:32:06 -0400

    added description section under thumbnail and edited stylesheet

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Store Conten' });
+  res.render('shop/index', { title: 'Store Content' });
 });
 
 module.exports = router;
