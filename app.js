996e771c394c594c3ae8252b509f94ad19cb192f
jamaalg <jamaal1087@gmail.com>
2017-04-28 10:44:01 -0400

    installed passport local

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var bodyParser = require('body-parser');
 var expresshbs = require('express-handlebars');
 var mongoose = require('mongoose');
 var session = require('express-session');
-
-var index = require('./routes/index');
+var passport = require('passport');
+var flash = require('connect-flash');
+var routes = require('./routes/index');
 
 
 var app = express();
@@ -18,6 +19,7 @@ mongoose.connect('localhost:27017/shopping');
 app.engine('.hbs', expresshbs({defaultLayout: 'layout', extname: '.hbs'}))
 app.set('view engine', '.hbs');
 
+// Middleware Initialization
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -25,9 +27,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
+
+// using of connect-flash and passport
+// needs sessions to be initialized first because it uses sessions
+app.use(flash());
+// initalize passport, set it to use sessions to store the users
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
