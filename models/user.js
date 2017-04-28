972c2672844b543a44745e14adb90fee98547eee
jamaalg <jamaal1087@gmail.com>
2017-04-27 23:33:31 -0400

    added user.js MODELS, defined mongoose schema for user signUp

---
+++ models/user.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var userSchema = new Schema({
+  email: {type: String, required: true},
+  password: {type: String, required: true},
+});
+
+// Specify name of model and schema
+module.exports = mongoose.model('User', userSchema);
