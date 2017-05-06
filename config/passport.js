84c796359bd4a6f8536bdc9462a288c0d6e8dbfc
jamaalg <jamaal1087@gmail.com>
2017-05-06 16:17:29 -0400

    added express-validator, fixed call back function causing error in user sign up

--- config/passport.js
+++ config/passport.js
@@ -17,8 +17,18 @@ passport.deserializeUser(function(id, done){
 passport.use('local.signup', new LocalStrategy({
   usernameField: 'email',
   passwordField: 'password',
-  passReqToCallback: true },function(req, email, password, done){
-
+  passReqToCallback: true
+},function(req, email, password, done){
+    req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+    req.checkBody('password', 'Invalid password').notEmpty().isLength({min:4});
+    var errors = req.validationErrors();
+    if (errors){
+      var messages =[];
+      errors.forEach(function(error){
+        messages.push(error.msg);
+      });
+      return done(null,false, req.flash('error', messages));
+    }
     // Query the database to find potential matches for user emails
     User.findOne({'email': email},function(err, user){
       if (err){
@@ -27,18 +37,15 @@ passport.use('local.signup', new LocalStrategy({
       if (user){
         return done(null, false, {message: 'Email is alredy in use'});
       }
-    });
-
-
-    // Creates new user
-    var newUser = new User();
-    newUser.email =email;
-    newUser.password = newUser.encryptPassword(password);
-    newUser.save(function(err,result){
-      if (err){
-        return done(err);
-      }
+      // Creates new user
+      var newUser = new User();
+      newUser.email =email;
+      newUser.password = newUser.encryptPassword(password);
+      newUser.save(function(err,result){
+        if (err){
+          return done(err);
+        }
         return done(null, newUser);
+      });
     });
-
 }));
