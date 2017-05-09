c199315db33e3c0e8824b11f9b66245b32a8762f
jamaalg <jamaal1087@gmail.com>
2017-05-09 09:18:49 -0400

    fixed cart issue re: quantity and price not updating

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var Cart = require('../models/cart');
 var Product =require('../models/product');
 
 router.get('/', function(req, res, next) {
@@ -14,6 +14,20 @@ router.get('/', function(req, res, next) {
       productChunks.push(docs.slice(i,i+chunkSize));
     }
     res.render('shop/index', { title: 'Store Content' , products: productChunks});
+  });
+});
+
+router.get('/add-to-cart/:id', function(req,res,next){
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart: {items: {}});
+  Product.findById(productId,function(err,product){
+    if (err){
+      return res.redirect('/');
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect('/');
   });
 });
 
