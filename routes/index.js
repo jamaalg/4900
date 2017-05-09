c9e9a70804d58dddceee4e4dddb82ab4d656fd6e
jamaalg <jamaal1087@gmail.com>
2017-05-09 09:42:19 -0400

    added shopping-cart route to index.js file & fixed link to shopping cart in the header view

--- routes/index.js
+++ routes/index.js
@@ -31,4 +31,15 @@ router.get('/add-to-cart/:id', function(req,res,next){
   });
 });
 
+
+router.get("shoppig-cart", function(req,res,next){
+  if(!req.session.cart){
+    return res.render('shop/shopping-cart', {products: bull});
+  }
+  var cart = new Cart(req.session.cart);
+  res.render('shop/shopping-cart',{products:cart.generateArray(), totalPrice: cart.totalPrice});
+
+});
+
+
 module.exports = router;
